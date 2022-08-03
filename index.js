0d45a8c16c10192cb19f787570b578de253e2940
Robert Bishop <rbishop85@gmail.com>
2022-08-02 23:08:05 -0700

    Created functions to use inquirer to ask questions about engineers and interns and console log results.  All 3 employee generating functions also call a function at their end that will ask which employee to generate next.  Created a function to be run when the page is launched that calls the manager generating function

--- index.js
+++ index.js
@@ -111,32 +111,74 @@ function generateManager() {
     .then((response) => {
     console.log(response);
 
-    // then Build a manager object and push to array list
+    // then build a manager object and push to array list
+
+    teamMemberChoice();
 
     })
 }
 
-        
-
-        
 
     // Gather engineer data
+function generateEngineer() {
+    // inquirer.prompt()
+    inquirer.prompt(questionsEngineer)
+    .then((response) => {
+    console.log(response);
 
-        // inquirer.prompt()
+    // then build an engineer object and push to array list
 
-        // then build an engineer object and push to array list
+    teamMemberChoice();
+
+    })
+}
 
     // gather intern data
+function generateIntern() {
+    // inquirer.prompt()
+    inquirer.prompt(questionsIntern)
+    .then((response) => {
+    console.log(response);
 
-        // inquirer.prompt()
+    // then build an intern object and push to array list
 
-        // then build an intern object and push to array list
+    teamMemberChoice();
+
+    })
+}
 
 
     // Ask for which team member they want to add or are they done (ends app and generates file when done)
-
-        // Inquirer.prompt()
-
+function teamMemberChoice() {
+    // Inquirer.prompt()
+    inquirer.prompt([
+        {
+        type: 'list',
+        message: 'What type of team member would you like to add next?',
+        name: 'choice',
+        choices: ['Engineer', 'Intern', "None, I am done adding team members"],
+        prefix: '-'
+        }
+    ])
+    .then((response) => {
+        console.log(response);
         // Then decide which function to call
+        if(response.choice === "Engineer") {
+            generateEngineer();
+        } else if(response.choice === "Intern") {
+            generateIntern();
+        } else {
+            // Take gathered data and generate html page
+        }
+    })
+}
+
+function init() {
+    generateManager()
+}
+
+init();
+
+        
 
     // Generate the html contents and write it to a file
