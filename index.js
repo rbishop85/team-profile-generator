664ce242547d4fc721bd2e61719326db6c509deb
Robert Bishop <rbishop85@gmail.com>
2022-08-02 21:21:43 -0700

    Setup file dependencies, including installing inquirer and jest

---
+++ index.js
@@ -0,0 +1,8 @@
+const inquirer = require("inquirer")
+const fs = require("fs")
+const pageTemplate = require("./src/page-template")
+
+const Employee = require("./lib/Employee")
+const Engineer = require("./lib/Engineer")
+const Intern = require("./lib/Intern")
+const Manager = require("./lib/Manager")
