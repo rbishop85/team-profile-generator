3b315d40b016384e1bd6db81e06483aa0d7faba8
Robert Bishop <rbishop85@gmail.com>
2022-08-02 21:45:31 -0700

    setup empty array for holding employee objects

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const inquirer = require("inquirer")
-const fs = require("fs")
-const pageTemplate = require("./src/page-template")
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-const Employee = require("./lib/Employee")
-const Engineer = require("./lib/Engineer")
-const Intern = require("./lib/Intern")
-const Manager = require("./lib/Manager")
+const pageTemplate = require("./src/page-template");
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+const employeesArray = [];
