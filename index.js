597fa04238070f312e90b2e449a8528605517215
Robert Bishop <rbishop85@gmail.com>
2022-08-02 21:46:34 -0700

    copy in pseudo code from office hours

--- index.js
+++ index.js
@@ -8,3 +8,32 @@ const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
 const employeesArray = [];
+
+// separate everything into reusable functions so you can easily cycle back to different functions
+
+    // gather manager data
+
+        // inquirer.prompt()
+
+        // then Build a manager object and push to array list
+
+    // Gather engineer data
+
+        // inquirer.prompt()
+
+        // then build an engineer object and push to array list
+
+    // gather intern data
+
+        // inquirer.prompt()
+
+        // then build an intern object and push to array list
+
+
+    // Ask for which team member they want to add or are they done (ends app and generates file when done)
+
+        // Inquirer.prompt()
+
+        // Then decide which function to call
+
+    // Generate the html contents and write it to a file
